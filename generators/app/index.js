e9eae2da8d8ddd0cc11dbb896d0334f57d80c428
Ken Snyder <ken@ken.net>
2020-05-29 16:02:37 -0700

    manually adjusted JS index

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,30 +1,13 @@
-'use strict';
+"use strict";
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
-var Base = require('yeoman-generator');
-var chalk = require('chalk');
-
-class Generator extends Base {
-    constructor(args, opts) {
-        super(args, opts);
-    }
-    initializing() {
-        console.log(computerText);
-        console.log(chalk `\nStarting the {green Lambda Typescript} generator. {italic For AWS functions with attitude!}\n`);
-    }
-    install() {
-        install(this);
-    }
-}
-
-//#region dev-deps
+var Base = require("yeoman-generator");
+var chalk = require("chalk");
 const typings = ["@types/aws-sdk", "@types/lodash", "@types/rimraf", "@types/handlebars"];
 const serverlessDevDeps = [
-    "serverless",
-    "serverless-pseudo-parameters",
-    "serverless-step-functions",
-    "serverless-log-forwarding",
+  "serverless",
+  "serverless-pseudo-parameters",
+  "serverless-step-functions",
+  "serverless-log-forwarding",
 ];
 const webpackRelated = ["webpack", "webpack-bundle-analyzer", "webpack-cli"];
 const testingDevDeps = ["mocha", "chai", "@types/mocha", "@types/chai"];
@@ -35,12 +18,6 @@ const utilityDeps = ["date-fns", "common-types"];
 const serverlessDeps = ["aws-orchestrate", "aws-log", "aws-ssm"];
 const firebaseDeps = ["universal-fire", "firemodel"];
 //#endregion
-function install(ctx) {
-    const devDeps = [...typings, ...serverlessDevDeps, ...webpackRelated, ...testingDevDeps, ...otherDevDeps];
-    ctx.yarnInstall(devDeps, { dev: true });
-    const deps = [...utilityDeps, ...serverlessDeps, ...firebaseDeps];
-    ctx.yarnInstall(deps, { dev: false });
-}
 
 const computerText = `
 ┌──────────────────────┐
@@ -57,6 +34,24 @@ const computerText = `
 ***                  ***
 **              *******
 ****************\n`;
+
+class Generator extends Base {
+  constructor(args, opts) {
+    super(args, opts);
+  }
+  initializing() {
+    console.log(computerText);
+    console.log(chalk`\nStarting the {green Lambda Typescript} generator. {italic For AWS functions with attitude!}\n`);
+  }
+  install() {
+    const devDeps = [...typings, ...serverlessDevDeps, ...webpackRelated, ...testingDevDeps, ...otherDevDeps];
+    ctx.yarnInstall(devDeps, { dev: true });
+    const deps = [...utilityDeps, ...serverlessDeps, ...firebaseDeps];
+    ctx.yarnInstall(deps, { dev: false });
+  }
+}
+
+//#region dev-deps
 
 exports.Generator = Generator;
 exports.computerText = computerText;
